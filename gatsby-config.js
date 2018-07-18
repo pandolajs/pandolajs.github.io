8b4d799cd8845c3155971a44246c9aeb3e6f824e
sizhao <870301137@qq.com>
2018-07-19 01:28:49 +0800

    init: 使用 gatsby 初始化 pandolajs 静态站；

---
+++ gatsby-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Pandolajs!',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-less'
+  ],
+}
---
+++ src/components/footer/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import styles from './index.module.less'
+
+export default class Footer extends Component {
+  render () {
+    return (
+      <div className={styles.footer}>footer</div>
+    )
+  }
+}

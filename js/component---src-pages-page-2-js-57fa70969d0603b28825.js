a5b3d1b0535bdfa77f6036e6f96f2b8c97cb1776
sizhao <870301137@qq.com>
2018-07-26 09:45:59 +0800

    Updates

---
+++ js/component---src-pages-page-2-js-57fa70969d0603b28825.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{216:function(e,t,a){"use strict";a.r(t);var n=a(1),o=a.n(n),c=a(222),l=a(221);t.default=function(){return o.a.createElement(l.a,null,o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(c.a,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-57fa70969d0603b28825.js.map
---
+++ js/component---src-templates-post-js-2b69f7bfbb3d675aabcf.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{214:function(e,n,t){"use strict";t.r(n),t.d(n,"query",function(){return u});var a=t(1),r=t.n(a),l=t(221);n.default=function(e){var n=e.data.markdownRemark;return r.a.createElement(l.a,null,r.a.createElement("div",null,r.a.createElement("h1",null,n.frontmatter.title),r.a.createElement("div",{dangerouslySetInnerHTML:{__html:n.html}})))};var u="68260355"}}]);
+//# sourceMappingURL=component---src-templates-post-js-2b69f7bfbb3d675aabcf.js.map
